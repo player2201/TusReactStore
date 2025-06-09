d4bd7482126ab83953b12b1f7d3d243960aaabb6
Tu Thanh Nguyen <tunguyen220197@gmail.com>
2025-06-09 12:29:26 +0300

    Create the Entities Basket.cs and BasketItem.cs

---
+++ API/Entities/BasketItem.cs
@@ -0,0 +1,8 @@
+namespace API.Entities;
+public class BasketItem
+{
+    public int Id { get; set; }
+    public int Quantity { get; set; }
+    public int ProductId { get; set; }
+    public required Product Product { get; set; }
+}
