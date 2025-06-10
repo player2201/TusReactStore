1ed724ed9a23558869893613348b00a67699ac6b
Tu Thanh Nguyen <tunguyen220197@gmail.com>
2025-06-10 12:47:31 +0300

    Initialize BasketController.cs

--- API/Entities/Basket.cs
+++ API/Entities/Basket.cs
@@ -6,23 +6,39 @@ public class Basket
 {
     public int Id { get; set; }
     public required string BasketId { get; set; }
-    public List<BasketItem> Items { get; set; } = [];
+    public List<BasketItem> Items { get; set; } = new List<BasketItem>();
 
     public void AddItem(Product product, int quantity)
     {
-        if (product == null) ArgumentNullException.ThrowIfNull(product);
-        if (quantity <= 0) throw new ArgumentException("Quantity shoudl be greater than zero", nameof(quantity));
+        if (product == null)
+            ArgumentNullException.ThrowIfNull(product);
+        if (quantity <= 0)
+            throw new ArgumentException("Quantity should be greater than zero", nameof(quantity));
         var existingItem = FindItem(product.Id);
+        if (existingItem == null)
+        {
+            Items.Add(new BasketItem { Product = product, Quantity = quantity });
+        }
+        else
+        {
+            existingItem.Quantity += quantity;
+        }
     }
+
     public void RemoveItem(int productId, int quantity)
     {
-        if (quantity <= 0) throw new ArgumentException("Quantity should be greater than zero", nameof(quantity));
-     }
+        if (quantity <= 0)
+            throw new ArgumentException("Quantity should be greater than zero", nameof(quantity));
+        var item = FindItem(productId);
+        if (item == null)
+            return;
+        item.Quantity -= quantity;
+        if (item.Quantity <= 0)
+            Items.Remove(item);
+    }
 
     private BasketItem? FindItem(int productId)
     {
         return Items.FirstOrDefault(item => item.ProductId == productId);
     }
-
-
 }
