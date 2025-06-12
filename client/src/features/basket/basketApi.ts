9b1c9f986010e8bba43e6b8b9c38b2e8dc7e4ffd
Tu Thanh Nguyen <tunguyen220197@gmail.com>
2025-06-12 15:03:10 +0300

    Invalidating RTK Query cache. Now when adding items to the cart from catalog, we can instantly see the product in the cart

--- client/src/features/basket/basketApi.ts
+++ client/src/features/basket/basketApi.ts
@@ -5,9 +5,11 @@ import type { Basket } from "../../app/models/basket";
 export const basketApi = createApi({
   reducerPath: "basketApi",
   baseQuery: baseQueryWithErrorHandling,
+  tagTypes: ["Basket"],
   endpoints: (builder) => ({
     fetchBasket: builder.query<Basket, void>({
       query: () => "basket",
+      providesTags: ["Basket"],
     }),
     addBasketItem: builder.mutation<
       Basket,
@@ -17,6 +19,14 @@ export const basketApi = createApi({
         url: `basket?productId=${productId}&quantity=${quantity}`,
         method: "POST",
       }),
+      onQueryStarted: async (_, { dispatch, queryFulfilled }) => {
+        try {
+          await queryFulfilled;
+          dispatch(basketApi.util.invalidateTags(["Basket"]));
+        } catch (error) {
+          console.log(error);
+        }
+      },
     }),
     removeBasketItem: builder.mutation<
       void,
