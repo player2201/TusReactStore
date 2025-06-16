d0e7d63fde4824252f2d98c6ade7de2102d3c509
Tu Thanh Nguyen <tunguyen220197@gmail.com>
2025-06-16 17:29:19 +0300

    added checkout page.

--- client/src/app/shared/components/OrderSummary.tsx
+++ client/src/app/shared/components/OrderSummary.tsx
@@ -9,6 +9,7 @@ import {
 import { currencyFormat } from "../../../lib/util";
 import { useFetchBasketQuery } from "../../../features/basket/basketApi";
 import type { Item } from "../../models/basket";
+import { Link } from "react-router-dom";
 
 export default function OrderSummary() {
   const { data: basket } = useFetchBasketQuery();
@@ -58,10 +59,19 @@ export default function OrderSummary() {
         </Box>
 
         <Box mt={2}>
-          <Button variant="contained" color="primary" fullWidth sx={{ mb: 1 }}>
+          <Button
+            component={Link}
+            to="/checkout"
+            variant="contained"
+            color="primary"
+            fullWidth
+            sx={{ mb: 1 }}
+          >
             Checkout
           </Button>
-          <Button fullWidth>Continue Shopping</Button>
+          <Button component={Link} to="/catalog" fullWidth>
+            Continue Shopping
+          </Button>
         </Box>
       </Paper>
 
