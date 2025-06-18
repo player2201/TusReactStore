d1840e61fc10f0c7c9e9b91bbcbf263f19809e1d
Tu Thanh Nguyen <tunguyen220197@gmail.com>
2025-06-18 11:52:07 +0300

    Designed the layout of the filters

--- client/src/features/catalog/Filters.tsx
+++ client/src/features/catalog/Filters.tsx
@@ -1,7 +1,69 @@
+import {
+  Box,
+  Checkbox,
+  FormControl,
+  FormControlLabel,
+  FormGroup,
+  Paper,
+  Radio,
+  TextField,
+} from "@mui/material";
 import { useFetchFiltersQuery } from "./catalogApi";
+
+const sortOptions = [
+  { value: "name", label: "Alphabetical" },
+  { value: "priceDesc", label: "Price: High to Low" },
+  { value: "price", label: "Price: Low to High" },
+];
 
 export default function Filters() {
   const { data } = useFetchFiltersQuery();
   console.log(data);
-  return <div>Filters go here</div>;
+  return (
+    <Box display="flex" flexDirection="column" gap={3}>
+      <Paper>
+        <TextField label="Search products" variant="outlined" fullWidth />
+      </Paper>
+      <Paper sx={{ p: 3 }}>
+        <FormControl>
+          {sortOptions.map(({ value, label }) => (
+            <FormControlLabel
+              key={label}
+              control={<Radio sx={{ py: 0.7 }} />}
+              label={label}
+              value={value}
+            />
+          ))}
+        </FormControl>
+      </Paper>
+      <Paper sx={{ p: 3 }}>
+        <FormGroup>
+          {data &&
+            data.brands.map((item) => (
+              <FormControlLabel
+                key={item}
+                control={
+                  <Checkbox color="secondary" sx={{ py: 0.7, fontSize: 40 }} />
+                }
+                label={item}
+              />
+            ))}
+        </FormGroup>
+      </Paper>
+      <Paper sx={{ p: 3 }}>
+        <FormGroup>
+          {data &&
+            data.types.map((item) => (
+              <FormControlLabel
+                key={item}
+                control={
+                  <Checkbox color="secondary" sx={{ py: 0.7, fontSize: 40 }} />
+                }
+                label={item}
+              />
+            ))}
+        </FormGroup>
+      </Paper>
+    </Box>
+  );
 }
