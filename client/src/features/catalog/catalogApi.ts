9c478c9896a3057a372cb4f1b8e857f1af6cf7fd
Tu Thanh Nguyen <tunguyen220197@gmail.com>
2025-06-18 12:50:29 +0300

    Set up the Catalog RTK query API to send parameters with the request

--- client/src/features/catalog/catalogApi.ts
+++ client/src/features/catalog/catalogApi.ts
@@ -1,13 +1,20 @@
 import { createApi } from "@reduxjs/toolkit/query/react";
 import type { Product } from "../../app/models/product";
 import { baseQueryWithErrorHandling } from "../../app/api/baseApi";
+import type { ProductParams } from "../../app/models/productParams";
+import { filterEmptyValues } from "../../lib/util";
 
 export const catalogApi = createApi({
   reducerPath: "catalogApi",
   baseQuery: baseQueryWithErrorHandling,
   endpoints: (builder) => ({
-    fetchProducts: builder.query<Product[], void>({
-      query: () => ({ url: "products" }),
+    fetchProducts: builder.query<Product[], ProductParams>({
+      query: (productParams) => {
+        return {
+          url: "products",
+          params: filterEmptyValues(productParams),
+        };
+      },
     }),
     fetchProductDetails: builder.query<Product, number>({
       query: (productId) => `products/${productId}`,
