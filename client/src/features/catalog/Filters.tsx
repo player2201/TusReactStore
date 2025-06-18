effb3417c2213bc52004d01c290ac17b893fb55f
Tu Thanh Nguyen <tunguyen220197@gmail.com>
2025-06-18 14:14:17 +0300

    added ordering products alphabetically, price high to low, low to high functionality to the client

--- client/src/features/catalog/Filters.tsx
+++ client/src/features/catalog/Filters.tsx
@@ -1,14 +1,15 @@
 import {
   Box,
   Checkbox,
-  FormControl,
   FormControlLabel,
   FormGroup,
   Paper,
-  Radio,
 } from "@mui/material";
 import { useFetchFiltersQuery } from "./catalogApi";
 import Search from "./Search";
+import RadioButtonGroup from "../../app/shared/components/RadioButtonGroup";
+import { useAppDispatch, useAppSelector } from "../../app/store/store";
+import { setOrderBy } from "./catalogSlice";
 
 const sortOptions = [
   { value: "name", label: "Alphabetical" },
@@ -18,6 +19,9 @@ const sortOptions = [
 
 export default function Filters() {
   const { data } = useFetchFiltersQuery();
+  const { orderBy } = useAppSelector((state) => state.catalog);
+  const dispatch = useAppDispatch();
+
   console.log(data);
   return (
     <Box display="flex" flexDirection="column" gap={3}>
@@ -25,16 +29,11 @@ export default function Filters() {
         <Search />
       </Paper>
       <Paper sx={{ p: 3 }}>
-        <FormControl>
-          {sortOptions.map(({ value, label }) => (
-            <FormControlLabel
-              key={label}
-              control={<Radio sx={{ py: 0.7 }} />}
-              label={label}
-              value={value}
-            />
-          ))}
-        </FormControl>
+        <RadioButtonGroup
+          selectedValue={orderBy}
+          options={sortOptions}
+          onChange={(e) => dispatch(setOrderBy(e.target.value))}
+        />
       </Paper>
       <Paper sx={{ p: 3 }}>
         <FormGroup>
