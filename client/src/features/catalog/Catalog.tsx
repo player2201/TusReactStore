968fa911a47fdb90a0cc339e1462a8bf11e84fac
Tu Thanh Nguyen <tunguyen220197@gmail.com>
2025-06-18 11:19:26 +0300

    Catalog component is updated to use the filters

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,12 +1,19 @@
+import { Grid2 } from "@mui/material";
 import { useFetchProductsQuery } from "./catalogApi";
 import ProductList from "./ProductList";
+import Filters from "./Filters";
 
 export default function Catalog() {
   const { data, isLoading } = useFetchProductsQuery();
   if (isLoading || !data) return <div>Loading...</div>;
   return (
-    <>
-      <ProductList products={data} />
-    </>
+    <Grid2 container spacing={4}>
+      <Grid2 size={3}>
+        <Filters />
+      </Grid2>
+      <Grid2 size={9}>
+        <ProductList products={data} />
+      </Grid2>
+    </Grid2>
   );
 }
