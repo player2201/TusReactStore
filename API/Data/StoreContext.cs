8efdbe2334c8ed9af7d4247958173454f3f97700
Tu Thanh Nguyen <tunguyen220197@gmail.com>
2025-07-07 14:43:13 +0300

    Setup Identity in the API

--- API/Data/StoreContext.cs
+++ API/Data/StoreContext.cs
@@ -1,5 +1,6 @@
 using System;
 using API.Entities;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data;
@@ -9,4 +10,25 @@ public class StoreContext(DbContextOptions options) : DbContext(options)
     public required DbSet<Product> Products { get; set; }
     public required DbSet<Basket> Baskets { get; set; }
 
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder
+            .Entity<IdentityRole>()
+            .HasData(
+                new IdentityRole
+                {
+                    Id = "0cca4d67-1a7d-43f8-b0f9-e5fecaa34095",
+                    Name = "Member",
+                    NormalizedName = "MEMBER"
+                },
+                new IdentityRole
+                {
+                    Id = "21286596-bf46-49ad-9539-642433c3e04f",
+                    Name = "Admin",
+                    NormalizedName = "ADMIN"
+                }
+            );
+    }
 }
