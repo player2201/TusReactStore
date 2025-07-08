d3c88ed2b2bc403097a782948988ddb158088c9d
Tu Thanh Nguyen <tunguyen220197@gmail.com>
2025-07-08 17:13:13 +0300

    added RTK query api for the account

---
+++ client/src/features/account/accountApi.ts
@@ -0,0 +1,40 @@
+import { createApi } from "@reduxjs/toolkit/query/react";
+import { baseQueryWithErrorHandling } from "../../app/api/baseApi";
+import type { User } from "../../app/models/user";
+
+export const accountApi = createApi({
+  reducerPath: "accountApi",
+  baseQuery: baseQueryWithErrorHandling,
+  endpoints: (builder) => ({
+    login: builder.mutation<void, object>({
+      query: (creds) => {
+        return {
+          url: "login?useCookies=true",
+          method: "POST",
+          body: creds,
+        };
+      },
+    }),
+    register: builder.mutation<void, object>({
+      query: (creds) => {
+        return {
+          url: "account/register",
+          method: "POST",
+          body: creds,
+        };
+      },
+    }),
+    userInfo: builder.query<User, void>({
+      query: () => "account/user-info",
+    }),
+    logout: builder.mutation({
+      query: () => ({
+        url: "accoutn/logout",
+        method: "POST",
+      }),
+    }),
+  }),
+});
+
+export const { useLoginMutation, useRegisterMutation, useLogoutMutation } =
+  accountApi;
