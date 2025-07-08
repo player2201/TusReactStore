20dd7efb9f16fb9857f396e43971998bab33af93
Tu Thanh Nguyen <tunguyen220197@gmail.com>
2025-07-08 15:43:03 +0300

    added additional API endpoints for account in AccountController.cs

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,6 +1,7 @@
 using System;
 using API.DTOs;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -25,5 +26,33 @@ public class AccountController(SignInManager<User> signInManager) : BaseApiContr
         }
         await signInManager.UserManager.AddToRoleAsync(user, "Member");
         return Ok();
+    }
+
+    [HttpGet("user-info")]
+    public async Task<ActionResult> GetUserInfo()
+    {
+        if (User.Identity?.IsAuthenticated == false)
+            return NoContent();
+        var user = await signInManager.UserManager.GetUserAsync(User);
+
+        if (user == null)
+            return Unauthorized();
+        var roles = await signInManager.UserManager.GetRolesAsync(user);
+
+        return Ok(
+            new
+            {
+                user.Email,
+                user.UserName,
+                Roles = roles
+            }
+        );
+    }
+
+    [HttpPost("log-out")]
+    public async Task<ActionResult> Logout()
+    {
+        await signInManager.SignOutAsync();
+        return NoContent();
     }
 }
