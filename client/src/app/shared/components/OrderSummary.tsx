c2fe8811dd4590f8d9d98f3e1f0016aa73fa2c0a
Tu Thanh Nguyen <tunguyen220197@gmail.com>
2025-07-14 16:45:22 +0300

    Updating the checkout component. Now the checkout page has more information on the order.

--- client/src/app/shared/components/OrderSummary.tsx
+++ client/src/app/shared/components/OrderSummary.tsx
@@ -9,7 +9,7 @@ import {
 import { currencyFormat } from "../../../lib/util";
 import { useFetchBasketQuery } from "../../../features/basket/basketApi";
 import type { Item } from "../../models/basket";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export default function OrderSummary() {
   const { data: basket } = useFetchBasketQuery();
@@ -19,6 +19,7 @@ export default function OrderSummary() {
       0
     ) ?? 0;
   const deliveryFee = subtotal > 10000 ? 0 : 500;
+  const location = useLocation();
 
   return (
     <Box
@@ -59,16 +60,18 @@ export default function OrderSummary() {
         </Box>
 
         <Box mt={2}>
-          <Button
-            component={Link}
-            to="/checkout"
-            variant="contained"
-            color="primary"
-            fullWidth
-            sx={{ mb: 1 }}
-          >
-            Checkout
-          </Button>
+          {!location.pathname.includes("checkout") && (
+            <Button
+              component={Link}
+              to="/checkout"
+              variant="contained"
+              color="primary"
+              fullWidth
+              sx={{ mb: 1 }}
+            >
+              Checkout
+            </Button>
+          )}
           <Button component={Link} to="/catalog" fullWidth>
             Continue Shopping
           </Button>
