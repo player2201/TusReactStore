3d0beae5878f40158ddbe95d462169067785cfc7
Tu Thanh Nguyen <tunguyen220197@gmail.com>
2025-07-17 13:23:15 +0300

    Added the update address functionality. When user change their address and press save as default address, the new address persists even after reloading the page.

--- client/src/features/checkout/CheckoutStepper.tsx
+++ client/src/features/checkout/CheckoutStepper.tsx
@@ -7,26 +7,56 @@ import {
   Step,
   StepLabel,
   Stepper,
+  Typography,
 } from "@mui/material";
-import { AddressElement, PaymentElement } from "@stripe/react-stripe-js";
+import {
+  AddressElement,
+  PaymentElement,
+  useElements,
+} from "@stripe/react-stripe-js";
 import { useState } from "react";
 import Review from "./Review";
-import { useFetchAddressQuery } from "../account/accountApi";
+import {
+  useFetchAddressQuery,
+  useUpdateUserAddressMutation,
+} from "../account/accountApi";
 import type { Address } from "../../app/models/user";
 
 const steps = ["Address", "Payment", "Review"];
 
 export default function CheckoutStepper() {
   const [activeStep, setActiveStep] = useState(0);
-  const { data: { name, ...restAddress } = {} as Address } =
+  const { data: { name, ...restAddress } = {} as Address, isLoading } =
     useFetchAddressQuery();
+  const [updateAddress] = useUpdateUserAddressMutation();
+  const [saveAddressChecked, setSaveAddressChecked] = useState(false);
+  const elements = useElements();
 
-  const handleNext = () => {
+  const handleNext = async () => {
+    if (activeStep === 0 && saveAddressChecked && elements) {
+      const address = await getStripeAddress();
+      if (address) await updateAddress(address);
+    }
     setActiveStep((step) => step + 1);
   };
+
+  const getStripeAddress = async () => {
+    const addressElement = elements?.getElement("address");
+    if (!addressElement) return null;
+    const {
+      value: { name, address },
+    } = await addressElement.getValue();
+    if (name && address) return { ...address, name };
+    return null;
+  };
+
   const handleBack = () => {
     setActiveStep((step) => step - 1);
   };
+
+  if (isLoading)
+    return <Typography variant="h6">Loading checkout...</Typography>;
+
   return (
     <Paper sx={{ p: 3, borderRadius: 3 }}>
       <Stepper activeStep={activeStep}>
@@ -51,7 +81,12 @@ export default function CheckoutStepper() {
           />
           <FormControlLabel
             sx={{ display: "flex", justifyContent: "end" }}
-            control={<Checkbox />}
+            control={
+              <Checkbox
+                checked={saveAddressChecked}
+                onChange={(e) => setSaveAddressChecked(e.target.checked)}
+              />
+            }
             label="Save as default address"
           />
         </Box>
