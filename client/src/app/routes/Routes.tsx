5ab3eccaa183272359fc378e30256aebbd889992
Tu Thanh Nguyen <tunguyen220197@gmail.com>
2025-07-21 17:04:57 +0300

    Created an OrderDetailedPage to view an order in details.

--- client/src/app/routes/Routes.tsx
+++ client/src/app/routes/Routes.tsx
@@ -14,6 +14,7 @@ import RegisterForm from "../../features/account/RegisterForm";
 import RequireAuth from "./RequireAuth";
 import CheckoutSuccess from "../../features/checkout/CheckoutSuccess";
 import OrdersPage from "../../features/orders/OrdersPage";
+import OrderDetailedPage from "../../features/orders/OrderDetailedPage";
 
 export const router = createBrowserRouter([
   {
@@ -26,6 +27,7 @@ export const router = createBrowserRouter([
           { path: "checkout", element: <CheckoutPage /> },
           { path: "checkout/success", element: <CheckoutSuccess /> },
           { path: "orders", element: <OrdersPage /> },
+          { path: "orders/:id", element: <OrderDetailedPage /> },
         ],
       },
       { path: "", element: <HomePage /> },
